6a6eb211ff8e7752a3911eb1bc9bf19e2fd2eb93
Kayla Morris <moreiskay14@gmail.com>
2021-09-01 00:33:25 -0700

    Updated license functions & READMe.md

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license !== "None") {
-    return `[![License: MIT](https://img.shields.io/badge/License-${license}-yellow.svg)]`
+  if (license === "MIT") {
+    return `[![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)]`
+  }
+  if(license === "Apache 2.0") {
+    return `[![License: ${license}](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`
+  }
+  if (license === "ISC"){
+    return `[![License: ${license}](https://img.shields.io/badge/License-ISC-blue.svg)]`
   }
   return '';
 }
@@ -11,7 +17,7 @@ console.log("loading")
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license !== "None") {
-    return ` (https://opensource.org/licenses/MIT)`
+    return ` (https://opensource.org/licenses/${license})`
   }
   return '';
 }
@@ -39,18 +45,19 @@ function generateMarkdown(data) {
   ## Table of Contents
 - [Installation](#installation)
 - [Usage](#usage)
-${renderLicenseLink(data.license)}
 - [Credits](#credits)
 - [License](#license)
   ## Installation
   ${data.install}
-  ## Instructions for use
+  ## Usage
   ${data.usage}
+  ${renderLicenseLink(data.license)}
   ${renderLicenseSection(data.license)}
   ${renderLicenseBadge(data.license)}
   ${data.license}
-  ## Github account:${data.username}
-  ##Contact Me
+  ## Github account:
+  ${data.username}
+  ## Contact Me
   Please send any questions, comments, or concerns to ${data.email}
 `;
 }
