4ae2a63ce0f4038c0de56ee6984ddaeaf47d0a41
Kayla Morris <moreiskay14@gmail.com>
2021-08-31 19:41:16 -0700

    Added functions to render licenses

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,52 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None")
+  return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)]`
+}{
+  return ''
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license !== "None")
+  return `(https://opensource.org/licenses/Apache-2.0)`
+}
+else{
+  return ''
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license !== "None")
+
+    return ''
+  }
+{
+    return ''
+  }
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${renderMainTitle(data.title)}
-${renderLicenseSection(data.license)}
+  return `
+  # ${data.title}
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.location}
+  ## License
+  ${data.license}
+  ${renderLicenseBadge}
+  ${renderLicenseLink}
+  ${renderLicenseSection}
+  ## Credits
+  List Collaborators, if any:
+
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown
